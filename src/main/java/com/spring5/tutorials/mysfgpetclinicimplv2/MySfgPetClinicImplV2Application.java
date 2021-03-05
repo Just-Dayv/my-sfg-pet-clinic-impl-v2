df5ebac9642826a6e94982b87479eab80d80ddca
David <iyanuoluwaopa@gmail.com>
2021-03-05 11:04:31 +0100

    Initial commit Created multi modules

---
+++ src/main/java/com/spring5/tutorials/mysfgpetclinicimplv2/MySfgPetClinicImplV2Application.java
@@ -0,0 +1,13 @@
+package com.spring5.tutorials.mysfgpetclinicimplv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySfgPetClinicImplV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySfgPetClinicImplV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring5/tutorials/mysfgpetclinicimplv2/MySfgPetClinicImplV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring5.tutorials.mysfgpetclinicimplv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySfgPetClinicImplV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
