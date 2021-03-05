665b7bc7dfce1882211c672206824348fba27565
David <iyanuoluwaopa@gmail.com>
2021-03-05 11:32:54 +0100

    Update the modules with dependencies

---
+++ pet-clinic-data/src/main/java/com/spring5/tutorials/mysfgpetclinicimplv2/model/Person.java
@@ -0,0 +1,4 @@
+package com.spring5.tutorials.mysfgpetclinicimplv2.model;
+
+public class Person {
+}
---
+++ pet-clinic-data/src/main/java/com/spring5/tutorials/mysfgpetclinicimplv2/model/Vet.java
@@ -0,0 +1,24 @@
+package com.spring5.tutorials.mysfgpetclinicimplv2.model;
+
+public class Vet extends Person {
+
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String lastName;
+}
